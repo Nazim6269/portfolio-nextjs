236c3940f5da3f0df707ff658a7a9548c0b31678
nazimuddin <nazimuddin102001@gmail.com>
2024-08-26 16:26:57 +0600

    completed apprach section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from '@/components/Approach';
 import Client from '@/components/Client';
 import Experience from '@/components/Experience';
 import Grid from '@/components/Grid';
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Client />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
